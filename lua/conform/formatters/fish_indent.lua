3703907dabb79867e934a57f645558c5aef88b41
MahanRahmati <mahan.rahmati@protonmail.com>
2023-08-27 21:55:30 +0330

    feat: add fish_indent

---
+++ lua/conform/formatters/fish_indent.lua
@@ -0,0 +1,8 @@
+---@type conform.FormatterConfig
+return {
+  meta = {
+    url = "https://fishshell.com/docs/current/cmds/fish_indent.html",
+    description = "Indent or otherwise prettify a piece of fish code.",
+  },
+  command = "fish_indent",
+}
