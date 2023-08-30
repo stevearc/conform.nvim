e6e99af64db3f364086aaf55b8b5854ccd62bac4
Seth Daniel <github.com@sethdaniel.org>
2023-08-29 20:30:25 -0500

    feat: add perlimports (#13)

---
+++ lua/conform/formatters/perlimports.lua
@@ -0,0 +1,13 @@
+---@type conform.FileFormatterConfig
+return {
+  meta = {
+    url = "https://github.com/perl-ide/App-perlimports",
+    description = "Make implicit Perl imports explicit",
+  },
+  command = "perlimports",
+  args = {
+    "--read-stdin",
+    "--filename",
+    "$FILENAME",
+  },
+}
