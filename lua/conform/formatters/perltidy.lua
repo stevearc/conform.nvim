882b75994af34fed3c4fe6f1a97ad58b352ec25f
Seth Daniel <seth@sethdaniel.org>
2023-08-29 20:55:47 -0500

    feat: add perltidy (#12)

---
+++ lua/conform/formatters/perltidy.lua
@@ -0,0 +1,9 @@
+---@type conform.FileFormatterConfig
+return {
+  meta = {
+    url = "https://github.com/perltidy/perltidy",
+    description = "Perl::Tidy, a source code formatter for Perl",
+  },
+  command = "perltidy",
+  args = { "--quiet" },
+}
