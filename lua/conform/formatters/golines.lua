e1d68a58fa29d2a24f1a976c3c60521ffb31f32e
Seth Daniel <github.com@sethdaniel.org>
2023-08-29 20:31:09 -0500

    feat: add golines (#11)

---
+++ lua/conform/formatters/golines.lua
@@ -0,0 +1,8 @@
+---@type conform.FileFormatterConfig
+return {
+  meta = {
+    url = "https://github.com/segmentio/golines",
+    description = "A golang formatter that fixes long lines",
+  },
+  command = "golines",
+}
