a3118d81eed8f1451fb4e777bbc4f2ca126fa877
Seth Daniel <seth@sethdaniel.org>
2023-08-29 19:56:49 -0500

    feat: add shellharden

---
+++ lua/conform/formatters/shellharden.lua
@@ -0,0 +1,9 @@
+---@type conform.FileFormatterConfig
+return {
+  meta = {
+    url = "https://github.com/anordal/shellharden",
+    description = "The corrective bash syntax highlighter",
+  },
+  command = "shellharden",
+  args = { "--transform", "''" },
+}
