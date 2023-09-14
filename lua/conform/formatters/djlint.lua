ead025784c8e31b8e45016e620c2f17a13ff741a
zootedb0t <05kraken@proton.me>
2023-09-14 23:34:45 +0530

    feat: add djlint (#47)

---
+++ lua/conform/formatters/djlint.lua
@@ -0,0 +1,12 @@
+---@type conform.FileFormatterConfig
+return {
+  meta = {
+    url = "https://github.com/Riverside-Healthcare/djLint",
+    description = "✨ HTML Template Linter and Formatter. Django - Jinja - Nunjucks - Handlebars - GoLang.",
+  },
+  command = "djlint",
+  args = {
+    "--reformat",
+    "-",
+  },
+}
