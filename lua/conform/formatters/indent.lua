937d788a32d4a711f315a25ae52f1a6d2e98e989
Thim Cederlund <thim@cederlund.de>
2023-09-15 17:48:38 +0200

    feat: add GNU/BSD indent

---
+++ lua/conform/formatters/indent.lua
@@ -0,0 +1,9 @@
+---@type conform.FileFormatterConfig
+return {
+  meta = {
+    url = "https://www.gnu.org/software/indent/",
+    description = "GNU Indent",
+  },
+  command = "indent",
+  stdin = true,
+}
