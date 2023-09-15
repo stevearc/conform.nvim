508ec8a899e039a56f9110011125ab56284db1fa
Steven Arcangeli <stevearc@stevearc.com>
2023-09-15 08:37:43 -0700

    feat: add shellcheck (#44)

---
+++ lua/conform/formatters/shellcheck.lua
@@ -0,0 +1,10 @@
+---@type conform.FileFormatterConfig
+return {
+  meta = {
+    url = "https://github.com/koalaman/shellcheck",
+    description = "A static analysis tool for shell scripts.",
+  },
+  command = "shellcheck",
+  args = "$FILENAME --format=diff | patch -p1 $FILENAME",
+  stdin = false,
+}
