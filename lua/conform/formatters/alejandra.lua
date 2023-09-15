e6552b5c9b3a2b12bacb476b00c80c736b9f7963
Leiser Fernández Gallo <leiserfg@gmail.com>
2023-09-15 15:48:05 +0200

    feat: alejandra formatter (#52)

---
+++ lua/conform/formatters/alejandra.lua
@@ -0,0 +1,8 @@
+---@type conform.FileFormatterConfig
+return {
+  meta = {
+    url = "https://kamadorueda.com/alejandra/",
+    description = "The Uncompromising Nix Code Formatter.",
+  },
+  command = "alejandra",
+}
