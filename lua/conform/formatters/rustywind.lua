a839ed1384c21cbd8861f2850b552a4db10ead2f
Eric Milford <ericmilford@gmail.com>
2023-09-15 22:33:43 -0500

    Feat: Add rustywind formatter (#56)
    * Feat: Add rustywind formatter
    
    This adds the rustywind, a CLI for organizing Tailwind CSS classes.
    
    * Update meta description

---
+++ lua/conform/formatters/rustywind.lua
@@ -0,0 +1,9 @@
+---@type conform.FileFormatterConfig
+return {
+  meta = {
+    url = "https://github.com/avencera/rustywind",
+    description = "A tool for formatting Tailwind CSS classes.",
+  },
+  command = "rustywind",
+  args = { "--stdin" },
+}
