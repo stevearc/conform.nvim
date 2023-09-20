74d0ecd44340cec772db46f70be8e8721b83fef4
Stefan VanBuren <stefan@vanburen.xyz>
2023-09-20 09:40:58 -0400

    Change buf format URL

--- lua/conform/formatters/buf.lua
+++ lua/conform/formatters/buf.lua
@@ -1,7 +1,7 @@
 ---@type conform.FileFormatterConfig
 return {
   meta = {
-    url = "https://buf.build/docs/lint/overview",
+    url = "https://buf.build/docs/reference/cli/buf/format",
     description = "A new way of working with Protocol Buffers",
   },
   command = "buf",
