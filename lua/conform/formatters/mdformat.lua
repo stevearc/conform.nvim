4a4c92715b174b847ba0fcdccf9dfea71c8ed33e
Ryan Preston <47462344+rnprest@users.noreply.github.com>
2023-09-21 20:28:59 -0400

    feat: add mdformat (#68)
    * feat(formatters): add mdformat for markdown formatting
    
    * docs: add mdformat to readme and conform.txt

---
+++ lua/conform/formatters/mdformat.lua
@@ -0,0 +1,9 @@
+---@type conform.FileFormatterConfig
+return {
+  meta = {
+    url = "https://github.com/executablebooks/mdformat",
+    description = "An opinionated Markdown formatter.",
+  },
+  command = "mdformat",
+  args = { "-" },
+}
