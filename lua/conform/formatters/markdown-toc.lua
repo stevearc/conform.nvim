e799f688d34ec413544d5fd85e86a8b8d6a690b4
pseudometa <73286100+chrisgrieser@users.noreply.github.com>
2023-09-25 18:34:24 +0200

    fix: correct indent when formatting in different buffer

--- lua/conform/formatters/markdown-toc.lua
+++ lua/conform/formatters/markdown-toc.lua
@@ -6,10 +6,10 @@ return {
   },
   command = "markdown-toc",
   stdin = false,
-  args = function()
+  args = function(ctx)
     -- use the indentation set in the current buffer, effectively allowing us to
     -- use values from .editorconfig
-    local indent = vim.bo.expandtab and (" "):rep(vim.bo.tabstop) or "\t"
+    local indent = vim.bo[ctx.buf].expandtab and (" "):rep(vim.bo[ctx.buf].tabstop) or "\t"
     return { "--indent=" .. indent, "-i", "$FILENAME" }
   end,
 }
