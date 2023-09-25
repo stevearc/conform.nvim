de58b06d434047c6ecd5ec2d52877335d37b05fd
pseudometa <73286100+chrisgrieser@users.noreply.github.com>
2023-09-25 19:56:15 +0200

    feat: add `markdown-toc` (#75)
    * feat: add `markdown-toc`
    
    * fix: ran stylua
    
    * fix: correct indent when formatting in different buffer

---
+++ lua/conform/formatters/markdown-toc.lua
@@ -0,0 +1,15 @@
+---@type conform.FileFormatterConfig
+return {
+  meta = {
+    url = "https://github.com/jonschlinkert/markdown-toc",
+    description = "API and CLI for generating a markdown TOC (table of contents) for a README or any markdown files.",
+  },
+  command = "markdown-toc",
+  stdin = false,
+  args = function(ctx)
+    -- use the indentation set in the current buffer, effectively allowing us to
+    -- use values from .editorconfig
+    local indent = vim.bo[ctx.buf].expandtab and (" "):rep(vim.bo[ctx.buf].tabstop) or "\t"
+    return { "--indent=" .. indent, "-i", "$FILENAME" }
+  end,
+}
