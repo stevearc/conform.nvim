e691ecaf41139a68ccb79fde824cb534ca11abd2
nedia <aiden@nedia.dev>
2023-09-27 16:42:29 +1300

    feat: Add support for php-cs-fixer (#78)
    * feat(php-cs-fixer): Add support for php-cs-fixer
    
    * chore: Update README
    
    * fix(php-cs-fixer): Include meta
    
    * fix(php-cs-fixer): And include type annotation
    
    * fix(php-cs-fixer): Use find_executable instead of path_or

---
+++ lua/conform/formatters/php_cs_fixer.lua
@@ -0,0 +1,15 @@
+local util = require("conform.util")
+
+---@type conform.FileFormatterConfig
+return {
+  meta = {
+    url = "https://github.com/PHP-CS-Fixer/PHP-CS-Fixer",
+    description = "The PHP Coding Standards Fixer.",
+  },
+  command = util.find_executable({
+    "tools/php-cs-fixer/vendor/bin/php-cs-fixer",
+    "vendor/bin/php-cs-fixer",
+  }, "php-cs-fixer"),
+  args = { "fix", "$FILENAME" },
+  stdin = false,
+}
