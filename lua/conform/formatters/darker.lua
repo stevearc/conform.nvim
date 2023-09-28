f92c8a9c383292c3f81323f77f270e68996e57b3
Oula Kuuva <oula.kuuva@iki.fi>
2023-09-28 10:00:09 +0300

    fix: darker args
    Added --no-color since we don't want color output. Also set --revision
    and --stdin-filename since running darker for a single file doesn't make
    much sense without them.

--- lua/conform/formatters/darker.lua
+++ lua/conform/formatters/darker.lua
@@ -6,7 +6,15 @@ return {
     description = "Run black only on changed lines.",
   },
   command = "darker",
-  args = { "--stdout", "--quiet", "$FILENAME" },
+  args = {
+    "--quiet",
+    "--no-color",
+    "--stdout",
+    "--revision",
+    "HEAD..:STDIN:",
+    "--stdin-filename",
+    "$FILENAME",
+  },
   cwd = util.root_file({
     -- https://github.com/akaihola/darker#customizing-darker-black-isort-flynt-and-linter-behavior
     "pyproject.toml",
