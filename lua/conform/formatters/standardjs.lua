971fa7f2e4005454ce141ca8ee0462a3c34d2922
Dennis B <bluz71@users.noreply.github.com>
2023-09-28 16:32:37 +1000

    feat: add 'JavaScript Standard Style' formatter (#82)

---
+++ lua/conform/formatters/standardjs.lua
@@ -0,0 +1,11 @@
+local util = require("conform.util")
+
+---@type conform.FileFormatterConfig
+return {
+  meta = {
+    url = "https://standardjs.com",
+    description = "JavaScript Standard style guide, linter, and formatter.",
+  },
+  command = util.from_node_modules("standard"),
+  args = { "--fix", "--stdin", "$FILENAME" },
+}
