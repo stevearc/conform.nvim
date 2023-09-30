37d036704a100ef6e6457be45b4dfc2f8e429572
Kerem Bozdas <krmbzds.github@gmail.com>
2023-09-30 20:49:34 +0300

    feat: add standardrb (#91)
    * feat: add standardrb formatter
    
    * [docgen] Update docs
    skip-checks: true
    
    ---------
    
    Co-authored-by: Github Actions <actions@github>

---
+++ lua/conform/formatters/standardrb.lua
@@ -0,0 +1,17 @@
+---@type conform.FileFormatterConfig
+return {
+  meta = {
+    url = "https://github.com/standardrb/standard",
+    description = "Ruby's bikeshed-proof linter and formatter",
+  },
+  command = "standardrb",
+  args = {
+    "--fix",
+    "-f",
+    "quiet",
+    "--stderr",
+    "--stdin",
+    "$FILENAME",
+  },
+  exit_codes = { 0, 1 },
+}
