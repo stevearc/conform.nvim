e8877369df244515af20e18bf1307632fc638d2a
Noah Zhu <virezox@gmail.com>
2023-10-02 10:31:54 -0500

    feat: add 'google-java-format' formatter (#99)
    Co-authored-by: Noah Zhu <noah@Noahs-M2-MacBook-Pro-16.local>

---
+++ lua/conform/formatters/google-java-format.lua
@@ -0,0 +1,9 @@
+---@type conform.FileFormatterConfig
+return {
+  meta = {
+    url = "https://github.com/google/google-java-format",
+    description = "Reformats Java source code according to Google Java Style.",
+  },
+  command = "google-java-format",
+  args = { "-" },
+}
