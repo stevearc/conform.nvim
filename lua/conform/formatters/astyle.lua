6e19ad54d883b557228c94e19b910993469125df
Rishabh <53911515+Rishabh672003@users.noreply.github.com>
2023-10-02 15:24:41 +0530

    feat : add astyle formatter

---
+++ lua/conform/formatters/astyle.lua
@@ -0,0 +1,14 @@
+local util = require("conform.util")
+---@type conform.FileFormatterConfig
+return {
+  meta = {
+    url = "https://astyle.sourceforge.net/astyle.html",
+    description = "A Free, Fast, and Small Automatic Formatter for C, C++, C++/CLI, Objective-C, C#, and Java Source Code",
+  },
+  command = "astyle",
+  args = { "--quiet" },
+  cwd = util.root_file({
+    ".astylerc",
+    "_astylerc",
+  }),
+}
