29bd52d4c4fbc05e1839fdc1e6b159fdfc4561da
rawalplawit <39916917+rawalplawit@users.noreply.github.com>
2023-10-03 19:15:45 +0545

    feat: add phpcbf

---
+++ lua/conform/formatters/phpcbf.lua
@@ -0,0 +1,19 @@
+local util = require("conform.util")
+
+---@type conform.FileFormatterConfig
+return {
+  meta = {
+    url = "https://phpqa.io/projects/phpcbf.html",
+    description = "PHP Code Beautifier and Fixer fixes violations of a defined coding standard.",
+  },
+  command = util.find_executable({
+    "vendor/bin/phpcbf",
+  }, "phpcbf"),
+  args = { "-q", "--stdin-path=", "$FILENAME", "-" },
+  stdin = true,
+  -- 0: no errors found
+  -- 1: errors found
+  -- 2: fixable errors found
+  -- 3: processing error
+  exit_codes = { 0, 1, 2 },
+}
