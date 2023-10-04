d413248186c84ff1ecb9f946fa367ab4e338b6cd
Ethan Gunderson <ethan@ethangunderson.com>
2023-10-03 21:12:33 -0500

    split command and set cwd

--- lua/conform/formatters/mix.lua
+++ lua/conform/formatters/mix.lua
@@ -4,6 +4,10 @@ return {
     url = "https://hexdocs.pm/mix/main/Mix.Tasks.Format.html",
     description = "Format Elixir files using the mix format command.",
   },
-  command = "mix format",
-  args = { "$FILENAME" },
+  command = "mix",
+  args = { "format", "-" },
+  cwd = require("conform.util").root_file({
+    ".formatter.exs",
+    "mix.exs",
+  }),
 }
