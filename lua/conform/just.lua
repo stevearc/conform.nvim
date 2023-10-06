4c91b5270a6f741850de2eef3a804ff1dc6ec3ee
BADR <contact@pythops.com>
2023-10-06 02:16:12 +0200

    feat: add justfile formatter (#114)

---
+++ lua/conform/just.lua
@@ -0,0 +1,9 @@
+return {
+  meta = {
+    url = "https://github.com/casey/just",
+    description = "Format Justfile",
+  },
+  command = "just",
+  args = { "--fmt", "--unstable", "-f", "$FILENAME" },
+  stdin = false,
+}
