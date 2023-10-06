e5ed0635d9aa66c6c2f7eac3235e6a8eb2de0653
pseudometa <73286100+chrisgrieser@users.noreply.github.com>
2023-10-06 18:45:14 +0200

    fix(biome): do not use stdin due to biome bug (#120)

--- lua/conform/formatters/biome.lua
+++ lua/conform/formatters/biome.lua
@@ -5,6 +5,10 @@ return {
     description = "A toolchain for web projects, aimed to provide functionalities to maintain them.",
   },
   command = "biome",
-  stdin = true,
-  args = { "format", "--stdin-file-path", "$FILENAME" },
+
+  -- pending this bug, do not use stdin: https://github.com/biomejs/biome/issues/455
+  stdin = false,
+  args = { "format", "--write", "$FILENAME" },
+  -- stdin = true,
+  -- args = { "format", "--stdin-file-path", "$FILENAME" },
 }
