b42d291a9dd05ec845499a56823ee3fb4728bb3c
Estifanos <95101485+justEstif@users.noreply.github.com>
2023-10-11 16:46:33 -0400

    feat: shorter description

--- lua/conform/formatters/rufo.lua
+++ lua/conform/formatters/rufo.lua
@@ -1,7 +1,7 @@
 return {
   meta = {
     url = "https://github.com/ruby-formatter/rufo",
-    description = "Rufo is as an opinionated ruby formatter, intended to be used via the command line as a text-editor plugin, to autoformat files on save or on demand.",
+    description = "Rufo is as an opinionated ruby formatter.",
   },
   command = "rufo",
   exit_codes = { 0, 3 },
