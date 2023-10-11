86065911c7abf0ab0f63ccaf43da1b1e700f0538
Estifanos <95101485+justEstif@users.noreply.github.com>
2023-10-11 16:44:42 -0400

    feat: add rufo support

---
+++ lua/conform/formatters/rufo.lua
@@ -0,0 +1,8 @@
+return {
+  meta = {
+    url = "https://github.com/ruby-formatter/rufo",
+    description = "Rufo is as an opinionated ruby formatter, intended to be used via the command line as a text-editor plugin, to autoformat files on save or on demand.",
+  },
+  command = "rufo",
+  exit_codes = { 0, 3 },
+}
