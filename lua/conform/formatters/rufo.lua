aca5d307232a22600bd0ab57571a8b6e2dc9a12c
Estifanos Beyene <95101485+justEstif@users.noreply.github.com>
2023-10-11 17:57:53 -0400

    feat: add rufo support (#132)
    * feat: add rufo support
    
    * feat: shorter description

---
+++ lua/conform/formatters/rufo.lua
@@ -0,0 +1,8 @@
+return {
+  meta = {
+    url = "https://github.com/ruby-formatter/rufo",
+    description = "Rufo is as an opinionated ruby formatter.",
+  },
+  command = "rufo",
+  exit_codes = { 0, 3 },
+}
