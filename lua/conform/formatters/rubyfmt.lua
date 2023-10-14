ae337775e46804a8347ea7c3da92be5587e5850e
Howard Huang <280972+hahuang65@users.noreply.github.com>
2023-10-13 21:49:50 -0500

    feat: support for rubyfmt (#139)
    Co-authored-by: Github Actions <actions@github>

---
+++ lua/conform/formatters/rubyfmt.lua
@@ -0,0 +1,8 @@
+return {
+  meta = {
+    url = "https://github.com/fables-tales/rubyfmt",
+    description = "Ruby Autoformatter! (Written in Rust)",
+  },
+  command = "rubyfmt",
+  exit_codes = { 0, 1 },
+}
