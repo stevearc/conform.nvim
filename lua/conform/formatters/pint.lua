574be08180c25c0f709ca48936dd36b9c1020219
Konstantin Auffinger <konstantinauffinger@gmail.com>
2023-10-16 17:02:44 +0200

    fix: remove unnecessary mason executable path

--- lua/conform/formatters/pint.lua
+++ lua/conform/formatters/pint.lua
@@ -7,7 +7,6 @@ return {
     description = "Laravel Pint is an opinionated PHP code style fixer for minimalists. Pint is built on top of PHP-CS-Fixer and makes it simple to ensure that your code style stays clean and consistent.",
   },
   command = util.find_executable({
-    vim.fn.stdpath("data") .. "/mason/bin/pint",
     "vendor/bin/pint",
   }, "pint"),
   args = { "$FILENAME" },
