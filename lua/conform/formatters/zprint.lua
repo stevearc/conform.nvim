afc176281553f4d45c0ce4a6bc5105aa8208758d
Aron Griffis <aron@scampersand.com>
2023-10-17 07:22:56 -0400

    feat: zprint formatter for clojure

---
+++ lua/conform/formatters/zprint.lua
@@ -0,0 +1,17 @@
+---@type conform.FileFormatterConfig
+return {
+  meta = {
+    url = "https://github.com/kkinnear/zprint",
+    description = "Formatter for Clojure and EDN.",
+  },
+  command = "zprint",
+  range_args = function(ctx)
+    return {
+      string.format(
+        "{:input {:range {:start %d :end %d :use-previous-!zprint? true :continue-after-!zprint-error? true}}}",
+        ctx.range.start[1] - 1,
+        ctx.range["end"][1] - 1
+      ),
+    }
+  end,
+}
