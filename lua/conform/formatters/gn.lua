37169273a0776752a3c01cbe01227e275b642b89
H. James Zhao <1094310+hantianjz@users.noreply.github.com>
2023-10-22 01:04:33 -0400

    feat: gn build file format cmd (#155)

---
+++ lua/conform/formatters/gn.lua
@@ -0,0 +1,9 @@
+---@type conform.FileFormatterConfig
+return {
+  meta = {
+    url = "https://gn.googlesource.com/gn/",
+    description = "gn build system",
+  },
+  command = "gn",
+  args = { "format", "--stdin" },
+}
