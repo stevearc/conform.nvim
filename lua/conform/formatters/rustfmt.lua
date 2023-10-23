23caaec1785220a86b6031ef425e6baeb8117d3a
Daniel Pecos Martínez <me@danielpecos.com>
2023-10-23 15:55:21 +0200

    rustfmt via cargo
    This will apply settings from Cargo.tml (like the rust edition to use)

--- lua/conform/formatters/rustfmt.lua
+++ lua/conform/formatters/rustfmt.lua
@@ -4,6 +4,6 @@ return {
     url = "https://github.com/rust-lang/rustfmt",
     description = "A tool for formatting rust code according to style guidelines.",
   },
-  command = "rustfmt",
-  args = { "--emit=stdout" },
+  command = "cargo",
+	args = { "fmt", "--", "-q", "--emit=stdout" },
 }
