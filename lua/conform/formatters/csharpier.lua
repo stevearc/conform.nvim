b2368ff18a9dd9452170d3a6f41b1f872ae5d0b2
Jesper Lundberg <lundberg.jesper@gmail.com>
2023-10-28 20:27:02 +0200

    feat: add CSharpier (#165)
    * Add formatter and update readme and conform.txt
    
    * Remove unused variable

---
+++ lua/conform/formatters/csharpier.lua
@@ -0,0 +1,10 @@
+---@type conform.FileFormatterConfig
+return {
+  meta = {
+    url = "https://github.com/belav/csharpier",
+    description = "The opinionated C# code formatter",
+  },
+  command = "dotnet-csharpier",
+  args = { "--write-stdout" },
+  stdin = true,
+}
