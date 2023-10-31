9bb3a940389dda796192a477a016069472692526
George Guimarães <george.guimaraes@gmail.com>
2023-10-31 11:29:31 -0300

    feat: add markdownlint-cli2 (#171)
    cli2 is a better cli for markdownlint. It allows to have config files in different directories, and it's also from the same author as the original node.js markdownlint lib.

---
+++ lua/conform/formatters/markdownlint-cli2.lua
@@ -0,0 +1,11 @@
+---@type conform.FileFormatterConfig
+return {
+  meta = {
+    url = "https://github.com/DavidAnson/markdownlint-cli2",
+    description = "A fast, flexible, configuration-based command-line interface for linting Markdown/CommonMark files with the markdownlint library",
+  },
+  command = "markdownlint-cli2",
+  args = { "--fix", "$FILENAME" },
+  exit_codes = { 0, 1 }, -- code 1 is returned when linting/formatter was successful and there were errors
+  stdin = false,
+}
