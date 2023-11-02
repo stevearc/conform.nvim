fdf6f8b3a8e716fea46abf070dd96a58be6c4951
Torsten Long <torsten.s.long@gmail.com>
2023-11-02 18:44:24 +0100

    feat: add mdslw markdown formatter

---
+++ lua/conform/formatters/mdslw.lua
@@ -0,0 +1,9 @@
+local util = require("conform.util")
+---@type conform.FileFormatterConfig
+return {
+  meta = {
+    url = "https://github.com/razziel89/mdslw",
+    description = "Prepare your markdown for easy diff'ing by adding line breaks after every sentence.",
+  },
+  command = "mdslw",
+}
