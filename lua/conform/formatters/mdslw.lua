369c7fe690b3fec0ecdd7c17faeebf3f8113a0f5
Torsten Long <razziel89@users.noreply.github.com>
2023-11-04 06:27:26 +0100

    feat: add mdslw markdown formatter (#175)
    * feat: add mdslw markdown formatter
    
    * lint: remove unused import
    
    ---------
    
    Co-authored-by: Torsten Long <torsten.s.long@gmail.com>
    Co-authored-by: Steven Arcangeli <506791+stevearc@users.noreply.github.com>

---
+++ lua/conform/formatters/mdslw.lua
@@ -0,0 +1,8 @@
+---@type conform.FileFormatterConfig
+return {
+  meta = {
+    url = "https://github.com/razziel89/mdslw",
+    description = "Prepare your markdown for easy diff'ing by adding line breaks after every sentence.",
+  },
+  command = "mdslw",
+}
