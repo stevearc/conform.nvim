fa3cf1c40716492fd0df0c3dedd54c8018f9ea70
pseudometa <73286100+chrisgrieser@users.noreply.github.com>
2023-11-05 15:29:20 +0100

    feat: add `ast-grep` (#177)

---
+++ lua/conform/formatters/astgrep.lua
@@ -0,0 +1,11 @@
+---@type conform.FileFormatterConfig
+return {
+  meta = {
+    url = "https://ast-grep.github.io/",
+    description = "A CLI tool for code structural search, lint and rewriting. Written in Rust",
+  },
+  command = "ast-grep",
+  args = { "scan", "--update-all", "$FILENAME" },
+  stdin = false,
+  exit_codes = { 0, 5 }, -- 5 = no config file exists
+}
