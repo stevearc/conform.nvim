b1f1194338c96d385ec6370ac734ab63c0289776
Håkon Harnes <89907156+HakonHarnes@users.noreply.github.com>
2023-11-06 17:26:11 +0100

    feat: add typstfmt (#180)

---
+++ lua/conform/formatters/typstfmt.lua
@@ -0,0 +1,9 @@
+---@type conform.FileFormatterConfig
+return {
+  meta = {
+    url = "https://github.com/astrale-sharp/typstfmt",
+    description = "Basic formatter for the Typst language with a future!",
+  },
+  command = "typstfmt",
+  stdin = true,
+}
