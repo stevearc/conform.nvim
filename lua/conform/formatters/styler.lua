018a91adfab95225507ab66b1586ee6e0744be1b
JohnCoene <jcoenep@gmail.com>
2023-11-10 09:00:01 +0100

    fix: lint styler formatter

--- lua/conform/formatters/styler.lua
+++ lua/conform/formatters/styler.lua
@@ -5,7 +5,7 @@ return {
     url = "https://github.com/devOpifex/r.nvim",
     description = "R formatter and linter.",
   },
-  command = util.find_executable({"usr/bin/"}, "R"),
-  args = {"-s", "-e", "r.nvim::format()", "--args", "$FILENAME"},
+  command = util.find_executable({ "usr/bin/" }, "R"),
+  args = { "-s", "-e", "r.nvim::format()", "--args", "$FILENAME" },
   stdin = false,
 }
