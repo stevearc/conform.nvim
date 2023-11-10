f0780e2231df2e4751e31db32c1545872412ba75
z775729168 <z775729168@gmail.com>
2023-11-10 08:31:26 +0800

    feat: add pangu (#188)

---
+++ lua/conform/formatters/pangu.lua
@@ -0,0 +1,9 @@
+---@type conform.FileFormatterConfig
+return {
+  meta = {
+    url = "https://github.com/vinta/pangu.py",
+    description = "Insert whitespace between CJK and half-width characters",
+  },
+  command = "pangu",
+  stdin = true,
+}
