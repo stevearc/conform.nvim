3622fc9c56b86067fbcd71e6517d4757ba265392
Varac <varac@varac.net>
2023-11-21 12:34:58 +0100

    feat: add beancount formatter

---
+++ lua/conform/formatters/bean-format.lua
@@ -0,0 +1,12 @@
+local util = require("conform.util")
+---@type conform.FileFormatterConfig
+return {
+  meta = {
+    url = "https://beancount.github.io/docs/running_beancount_and_generating_reports.html#bean-format",
+    description = "Reformat Beancount files to right-align all the numbers at the same, minimal column.",
+  },
+  command = "bean-format",
+  args = {
+    "-",
+  },
+}
