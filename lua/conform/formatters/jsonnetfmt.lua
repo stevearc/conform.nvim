347e7cca4e0ee11f49471c03f169db4c00d973e2
dadav <33197631+dadav@users.noreply.github.com>
2023-12-04 20:19:34 +0100

    docs: adjust url

--- lua/conform/formatters/jsonnetfmt.lua
+++ lua/conform/formatters/jsonnetfmt.lua
@@ -1,7 +1,7 @@
 ---@type conform.FileFormatterConfig
 return {
   meta = {
-    url = "https://github.com/google/go-jsonnet/cmd/jsonnetfmt",
+    url = "https://github.com/google/go-jsonnet/tree/master/cmd/jsonnetfmt",
     description = "jsonnetfmt is a command line tool to format jsonnet files.",
   },
   command = "jsonnetfmt",
