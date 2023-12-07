a5ef4943f6382f36a5a8d6e16eb0a0c60af5e7a5
Xun <58657914+eeeXun@users.noreply.github.com>
2023-12-08 00:46:51 +0800

    feat: add asmfmt (#239)

---
+++ lua/conform/formatters/asmfmt.lua
@@ -0,0 +1,9 @@
+---@type conform.FileFormatterConfig
+return {
+  meta = {
+    url = "https://github.com/klauspost/asmfmt",
+    description = "Go Assembler Formatter",
+  },
+  command = "asmfmt",
+  stdin = true,
+}
