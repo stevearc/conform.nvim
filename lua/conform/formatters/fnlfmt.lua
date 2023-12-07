b5fc65a0c59780040482dd2ed4a48ca09a839892
Marcin Puc <tranzystorek.io@protonmail.com>
2023-12-07 14:19:36 +0100

    feat: add fnlfmt formatter

---
+++ lua/conform/formatters/fnlfmt.lua
@@ -0,0 +1,9 @@
+--@type conform.FileFormatterConfig
+return {
+  meta = {
+    url = "https://git.sr.ht/~technomancy/fnlfmt",
+    description = "A formatter for Fennel code.",
+  },
+  command = "fnlfmt",
+  args = { "-" },
+}
