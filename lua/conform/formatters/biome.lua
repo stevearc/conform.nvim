1dd93c2631542b2cc548eb59fbe166ea20aaa71a
azzamsa <me@azzamsa.com>
2024-01-28 05:04:23 +0700

    fix: set a cwd for biome

--- lua/conform/formatters/biome.lua
+++ lua/conform/formatters/biome.lua
@@ -8,4 +8,7 @@ return {
   command = util.from_node_modules("biome"),
   stdin = true,
   args = { "format", "--stdin-file-path", "$FILENAME" },
+  cwd = util.root_file({
+    "biome.json",
+  }),
 }
