2e189df5cb1eaa573ce6989d62ef43e22daad167
Andreas Schneider <asn@cryptomilk.org>
2024-03-07 15:06:50 +0100

    fix: Fix range formatting of buffer
    Currently we provide a filename, which means prettier will format the
    given file and not what we feed via stdin.

--- lua/conform/formatters/prettier.lua
+++ lua/conform/formatters/prettier.lua
@@ -57,7 +57,7 @@ return {
   end,
   range_args = function(self, ctx)
     local start_offset, end_offset = util.get_offsets_from_range(ctx.buf, ctx.range)
-    local args = eval_parser(self, ctx) or { "$FILENAME" }
+    local args = eval_parser(self, ctx) or { "--stdin-filepath", "$FILENAME" }
     return vim.list_extend(args, { "--range-start=" .. start_offset, "--range-end=" .. end_offset })
   end,
   cwd = util.root_file({
