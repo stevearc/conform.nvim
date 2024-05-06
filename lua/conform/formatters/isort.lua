0d34c727caa6a38e0110cde8b39bb37a0bfc8d90
Steven Arcangeli <stevearc@stevearc.com>
2024-05-06 10:23:06 -0700

    fix(isort): explicitly pass line endings

--- lua/conform/formatters/isort.lua
+++ lua/conform/formatters/isort.lua
@@ -6,12 +6,26 @@ return {
     description = "Python utility / library to sort imports alphabetically and automatically separate them into sections and by type.",
   },
   command = "isort",
-  args = {
-    "--stdout",
-    "--filename",
-    "$FILENAME",
-    "-",
-  },
+  args = function(self, ctx)
+    -- isort doesn't do a good job of auto-detecting the line endings.
+    local line_ending
+    local file_format = vim.bo[ctx.buf].fileformat
+    if file_format == "dos" then
+      line_ending = "\r\n"
+    elseif file_format == "mac" then
+      line_ending = "\r"
+    else
+      line_ending = "\n"
+    end
+    return {
+      "--stdout",
+      "--line-ending",
+      line_ending,
+      "--filename",
+      "$FILENAME",
+      "-",
+    }
+  end,
   cwd = util.root_file({
     -- https://pycqa.github.io/isort/docs/configuration/config_files.html
     ".isort.cfg",
