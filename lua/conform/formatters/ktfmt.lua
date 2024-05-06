b72f650206ddfeadd6c7df0f775a928e82ece353
Alexandros Alexiou <alex.alexiouu@gmail.com>
2024-05-06 21:09:13 +0300

    feat: add ktfmt formatter for Kotlin (#392)

---
+++ lua/conform/formatters/ktfmt.lua
@@ -0,0 +1,9 @@
+---@type conform.FileFormatterConfig
+return {
+  meta = {
+    url = "https://github.com/facebook/ktfmt",
+    description = "Reformats Kotlin source code to comply with the common community standard conventions.",
+  },
+  command = "ktfmt",
+  args = { "-" },
+}
