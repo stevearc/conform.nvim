7a284b6a2879a289de77ed5f6fed2b3f10ded26d
Mads Hougesen <mads@mhouge.dk>
2024-05-09 18:55:25 +0200

    refactor: copy rustfmt to yew-fmt

--- lua/conform/formatters/yew-fmt.lua
+++ lua/conform/formatters/yew-fmt.lua
@@ -1,11 +1,16 @@
 -- yew-fmt is a fork of rustfmt
-local conf = vim.deepcopy(require("conform.formatters.rustfmt"))
+local rustfmt = require("conform.formatters.rustfmt")
 
-conf.meta = {
-  url = "https://github.com/schvv31n/yew-fmt",
-  description = "Code formatter for the Yew framework.",
+---@type conform.FileFormatterConfig
+return {
+  meta = {
+    url = "https://github.com/schvv31n/yew-fmt",
+    description = "Code formatter for the Yew framework.",
+  },
+  command = "yew-fmt",
+  options = {
+    -- The default edition of Rust to use when no Cargo.toml file is found
+    default_edition = rustfmt.default_edition,
+  },
+  args = rustfmt.args,
 }
-
-conf.command = "yew-fmt"
-
-return conf
