6207f41e8f3813d72ef4499a8132c11d8baabe9f
Marcus Fernandes <marcusbfs@gmail.com>
2024-05-09 23:52:22 -0300

    feat: add ormolu formatter for Haskell (#377) (#397)
    * feat: add ormolu formatter for Haskell (#377)
    
    * add ormolu references to README.md and doc/conform.txt
    
    * revert wrong auto typo fix by codespell
    
    * update meta field in the formatter
    
    * doc: typo
    
    ---------
    
    Co-authored-by: Steven Arcangeli <506791+stevearc@users.noreply.github.com>

--- lua/conform/formatters/fourmolu.lua
+++ lua/conform/formatters/fourmolu.lua
@@ -2,7 +2,7 @@
 return {
   meta = {
     url = "https://hackage.haskell.org/package/fourmolu",
-    description = "Fourmolu is a formatter for Haskell source code.",
+    description = "A fork of ormolu that uses four space indentation and allows arbitrary configuration.",
   },
   command = "fourmolu",
   args = { "--stdin-input-file", "$FILENAME" },
