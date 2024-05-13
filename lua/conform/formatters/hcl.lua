37cbceab0a3b4979c4f0f1ae7aede0d0fa84c1d1
Jonas Ohland <32197138+jonasohland@users.noreply.github.com>
2024-05-13 19:10:14 +0200

    feat: add hcl formatter (hclfmt) (#402)
    * feat: add hclfmt hcl formatter
    
    * doc: format description
    
    ---------
    
    Co-authored-by: Steven Arcangeli <506791+stevearc@users.noreply.github.com>

---
+++ lua/conform/formatters/hcl.lua
@@ -0,0 +1,8 @@
+---@type conform.FileFormatterConfig
+return {
+  meta = {
+    url = "https://github.com/hashicorp/hcl",
+    description = "A formatter for HCL files.",
+  },
+  command = "hclfmt",
+}
