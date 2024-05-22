be87363632ad95d1bcb60f183c80211ab52cbfa5
Steven Arcangeli <506791+stevearc@users.noreply.github.com>
2024-05-21 22:30:14 -0700

    doc: add period at end of description

--- lua/conform/formatters/typstyle.lua
+++ lua/conform/formatters/typstyle.lua
@@ -2,7 +2,7 @@
 return {
   meta = {
     url = "https://github.com/Enter-tainer/typstyle",
-    description = "Beautiful and reliable typst code formatter",
+    description = "Beautiful and reliable typst code formatter.",
   },
   command = "typstyle",
   stdin = true,
