b421e95a31e6c3b064a964292255e3b9c762fddd
Mads Hougesen <mads@mhouge.dk>
2024-06-01 21:04:26 +0200

    feat(lua): support lua-format (#432)
    * feat: add support for lua-format
    
    * refactor: use stdin instead of file

---
+++ lua/conform/formatters/lua-format.lua
@@ -0,0 +1,9 @@
+---@type conform.FileFormatterConfig
+return {
+  meta = {
+    url = "https://github.com/Koihik/LuaFormatter",
+    description = "Code formatter for Lua.",
+  },
+  command = "lua-format",
+  stdin = true,
+}
