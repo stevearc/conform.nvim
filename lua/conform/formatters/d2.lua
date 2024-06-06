5e7a000e4f239b56077e5a38680c5e9a0bf60e6a
giskard <rtgiskard@gmail.com>
2024-06-06 14:52:53 +0800

    feat: add support for d2 (#445)
    * feat: add support for d2
    
    * doc: normalize description
    
    ---------
    
    Co-authored-by: Steven Arcangeli <506791+stevearc@users.noreply.github.com>

---
+++ lua/conform/formatters/d2.lua
@@ -0,0 +1,9 @@
+---@type conform.FileFormatterConfig
+return {
+  meta = {
+    url = "https://github.com/terrastruct/d2",
+    description = "D2 is a modern diagram scripting language that turns text to diagrams.",
+  },
+  command = "d2",
+  args = { "fmt", "-" },
+}
