c26dadf8a47a547768d1048a0d698ecec33494ce
Steven Arcangeli <stevearc@stevearc.com>
2024-06-24 20:54:28 -0700

    fix(npm-groovy-lint): ignore exit code 1 (#477)

--- lua/conform/formatters/npm-groovy-lint.lua
+++ lua/conform/formatters/npm-groovy-lint.lua
@@ -6,5 +6,7 @@ return {
   },
   command = "npm-groovy-lint",
   args = { "--fix", "$FILENAME" },
+  -- https://github.com/nvuillam/npm-groovy-lint/blob/14e2649ff7ca642dba3e901c17252b178bea8b1b/lib/groovy-lint.js#L48
+  exit_codes = { 0, 1 }, -- 1 = expected error
   stdin = false,
 }
