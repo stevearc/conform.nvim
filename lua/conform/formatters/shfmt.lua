f0921fb0f6587e98dcafc2775814c58c348718cb
Micah Halter <micah@mehalter.com>
2024-06-28 09:24:19 -0400

    feat(shfmt): automatically detect indentation

--- lua/conform/formatters/shfmt.lua
+++ lua/conform/formatters/shfmt.lua
@@ -5,5 +5,16 @@ return {
     description = "A shell parser, formatter, and interpreter with `bash` support.",
   },
   command = "shfmt",
-  args = { "-filename", "$FILENAME" },
+  args = function(_, ctx)
+    local args = { "-filename", "$FILENAME" }
+    local bo = vim.bo[ctx.buf]
+    if bo.expandtab then
+      local indent_size = bo.shiftwidth
+      if indent_size == 0 or not indent_size then
+        indent_size = bo.tabstop or 2
+      end
+      vim.list_extend(args, { "-i", tostring(indent_size) })
+    end
+    return args
+  end,
 }
