f8b000a80664a9097572eea5962a75c53836afdf
Peter Aronoff <peter@aronoff.org>
2024-07-22 17:44:22 -0400

    feat: add sleek, an SQL formatter

---
+++ lua/conform/formatters/sleek.lua
@@ -0,0 +1,9 @@
+---@type conform.FileFormatterConfig
+return {
+  meta = {
+    url = "https://github.com/nrempel/sleek",
+    description = "Sleek is a CLI tool for formatting SQL.",
+  },
+  command = "sleek",
+  stdin = true,
+}
