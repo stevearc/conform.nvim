6c34a85334240329a503074b3fa32eea42d2f094
Ughur Alakbarov <58857108+ugur-a@users.noreply.github.com>
2024-07-30 15:50:42 +0200

    feat(formatters): add nixfmt-rfc-style
    docs: add nixfmt-rfc-style to readme and conform.txt

---
+++ lua/conform/formatters/nixfmt-rfc-style.lua
@@ -0,0 +1,8 @@
+---@type conform.FileFormatterConfig
+return {
+  meta = {
+    url = "https://github.com/NixOS/nixfmt",
+    description = "The official (but not yet stable) formatter for Nix code",
+  },
+  command = "nixfmt",
+}
