960f51becccadc36825b2c2db266c8cffaeadbde
sieuwerts <42545850+sieuwerts@users.noreply.github.com>
2024-08-07 07:27:03 +0200

    feat: add support for markdownfmt (#511)
    Co-authored-by: Christoffer Pettersson <christoffer.pettersson@schibsted.com>

---
+++ lua/conform/formatters/markdownfmt.lua
@@ -0,0 +1,8 @@
+---@type conform.FileFormatterConfig
+return {
+  meta = {
+    url = "https://github.com/shurcooL/markdownfmt",
+    description = "Like gofmt, but for Markdown.",
+  },
+  command = "markdownfmt",
+}
