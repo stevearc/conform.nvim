392fc98c475b4f0552078104624e509b4826d197
Tim Culverhouse <tim@timculverhouse.com>
2024-09-10 12:47:59 -0500

    feat(zine): add superhtml and ziggy support (#531)
    * feat(superhtml): support SuperHTML formatter
    
    Add support for SuperHTML formatter.
    
    * feat(ziggy): support Ziggy formatter
    
    Add support for Ziggy and Ziggy-Schema formatting.
    
    * doc: add trailing period to superhtml
    
    ---------
    
    Co-authored-by: Steven Arcangeli <506791+stevearc@users.noreply.github.com>

---
+++ lua/conform/formatters/ziggy.lua
@@ -0,0 +1,9 @@
+---@type conform.FileFormatterConfig
+return {
+  meta = {
+    url = "https://github.com/kristoff-it/ziggy",
+    description = "A data serialization language for expressing clear API messages, config files, etc.",
+  },
+  command = "ziggy",
+  args = { "fmt", "--stdin" },
+}
