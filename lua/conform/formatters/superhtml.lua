89e90cb74bb31a88a311084bd2e8acd874e564c0
Steven Arcangeli <506791+stevearc@users.noreply.github.com>
2024-09-10 10:47:06 -0700

    doc: add trailing period to superhtml

--- lua/conform/formatters/superhtml.lua
+++ lua/conform/formatters/superhtml.lua
@@ -2,7 +2,7 @@
 return {
   meta = {
     url = "https://github.com/kristoff-it/superhtml",
-    description = "HTML Language Server and Templating Language Library",
+    description = "HTML Language Server and Templating Language Library.",
   },
   command = "superhtml",
   args = { "fmt", "--stdin" },
