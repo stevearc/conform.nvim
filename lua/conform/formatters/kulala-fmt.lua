50f12a39bb9fab9e2f110d7acdd2559ae80a3ba8
Marco Kellershoff <marco@kellershoff.net>
2024-09-12 20:29:54 +0200

    feat: add kulala-fmt
    kulala-fmt is an opinionated .http and .rest files linter and formatter.

---
+++ lua/conform/formatters/kulala-fmt.lua
@@ -0,0 +1,10 @@
+---@type conform.FileFormatterConfig
+return {
+  meta = {
+    url = "https://github.com/mistweaverco/kulala-fmt",
+    description = "An opinionated .http and .rest files linter and formatter.",
+  },
+  command = "kulala-fmt",
+  args = { "$FILENAME" },
+  stdin = false,
+}
