a52d36dba68b08a448f178c646a508fdd5dd7169
Andreas Näsman <andreas.z.nasman@gmail.com>
2024-10-21 00:49:04 +0300

    Add warning for missing unstable flag (draft)

--- lua/conform/formatters/deno_fmt.lua
+++ lua/conform/formatters/deno_fmt.lua
@@ -1,3 +1,5 @@
+local log = require("conform.log")
+
 local extensions = {
   css = "css",
   esmodule = "mjs",
@@ -27,11 +29,24 @@ return {
   },
   command = "deno",
   args = function(self, ctx)
+    local extension = extensions[vim.bo[ctx.buf].filetype]
+
+    -- TODO: How do I check if the user has passed in `--unstable-component` with e.g. `append_args` in their config?
+    print("self: " .. vim.inspect(self))
+    print("ctx: " .. vim.inspect(ctx))
+    if true then
+      log.warn(
+        "You are trying to format an unstable file type (."
+          .. extension
+          .. ") without the corresponding `--unstable-component` flag. Add the flag to format the code. See the Deno documentation for more information: https://docs.deno.com/runtime/reference/cli/formatter/#formatting-options-unstable-component"
+      )
+    end
+
     return {
       "fmt",
       "-",
       "--ext",
-      extensions[vim.bo[ctx.buf].filetype],
+      extension,
     }
   end,
 }
