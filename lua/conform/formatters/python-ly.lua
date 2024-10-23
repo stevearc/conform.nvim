2df3e275d1a9ff4d3c73d7cbcc112d094432082b
Steven Arcangeli <506791+stevearc@users.noreply.github.com>
2024-10-22 22:47:15 -0700

    doc: normalize description format

--- lua/conform/formatters/python-ly.lua
+++ lua/conform/formatters/python-ly.lua
@@ -2,7 +2,7 @@
 return {
   meta = {
     url = "https://github.com/frescobaldi/python-ly",
-    description = "A Python package and commandline tool to manipulate LilyPond files ",
+    description = "A Python package and commandline tool to manipulate LilyPond files.",
   },
   command = "ly",
   args = { "reformat", "$FILENAME", "-o", "$FILENAME" },
