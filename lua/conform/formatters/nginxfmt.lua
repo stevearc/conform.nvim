7ab316bfd23ade279dd3e59642b9611f76dbcee5
D-Dev <dangdoan2206@gmail.com>
2024-10-31 17:41:07 +0700

    feat: add support for nginx-config-formatter

---
+++ lua/conform/formatters/nginxfmt.lua
@@ -0,0 +1,9 @@
+---@type conform.FileFormatterConfig
+return {
+  meta = {
+    url = "https://github.com/slomkowski/nginx-config-formatter",
+    description = "nginx config file formatter/beautifier written in Python with no additional dependencies.",
+  },
+  command = "nginxfmt",
+  args = { "-" },
+}
