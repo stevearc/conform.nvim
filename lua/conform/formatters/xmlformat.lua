d23765f50637529078aca879351a219d6b1d8010
Brage Ingebrigtsen <84551201+Skyppex@users.noreply.github.com>
2024-11-13 17:35:02 +0100

    fix(xmlformat): rename to  xmlformatter (#572)
    * fix(names): change name of xml formatter
    
    this makes the name consistent with other places where this program is mentioned such as mason.nvim or the name of the cli program itself
    
    * fix(formatters): add shim for xmlformatter

--- lua/conform/formatters/xmlformat.lua
+++ lua/conform/formatters/xmlformat.lua
@@ -1,9 +1,4 @@
----@type conform.FileFormatterConfig
-return {
-  meta = {
-    url = "https://github.com/pamoller/xmlformatter",
-    description = "xmlformatter is an Open Source Python package, which provides formatting of XML documents.",
-  },
-  command = "xmlformat",
-  args = { "-" },
-}
+-- This was renamed to xmlformatter
+local conf = vim.deepcopy(require("conform.formatters.xmlformatter"))
+conf.meta.deprecated = true
+return conf
---
+++ lua/conform/formatters/xmlformatter.lua
@@ -0,0 +1,9 @@
+---@type conform.FileFormatterConfig
+return {
+  meta = {
+    url = "https://github.com/pamoller/xmlformatter",
+    description = "xmlformatter is an Open Source Python package, which provides formatting of XML documents.",
+  },
+  command = "xmlformat",
+  args = { "-" },
+}
