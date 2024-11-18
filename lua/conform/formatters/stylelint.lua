76e06a7a46651b9e5fa4259167bafd4a334b6d52
Rick Harris <rickharris@users.noreply.github.com>
2024-11-17 22:02:06 -0800

    fix(stylelint): support ignored files, node_modules, and cwd

--- lua/conform/formatters/stylelint.lua
+++ lua/conform/formatters/stylelint.lua
@@ -1,11 +1,15 @@
+local util = require("conform.util")
 ---@type conform.FileFormatterConfig
 return {
   meta = {
     url = "https://github.com/stylelint/stylelint",
     description = "A mighty CSS linter that helps you avoid errors and enforce conventions.",
   },
-  command = "stylelint",
-  args = { "--stdin", "--fix" },
+  command = util.from_node_modules("stylelint"),
+  args = { "--stdin", "--stdin-filename", "$FILENAME", "--fix" },
   exit_codes = { 0, 2 }, -- code 2 is given when trying file includees some non-autofixable errors
   stdin = true,
+  cwd = util.root_file({
+    "package.json",
+  }),
 }
