d2fdcc3fa48cf2f2696f13730a5c74f97a7e9677
Fredrik Foss-Indrehus <fredrikfoss@fr.urbanpets.no>
2024-12-03 17:40:08 +0100

    feat: add tex-fmt (#593)
    Co-authored-by: Fredrik Foss-Indrehus <fred@ffoss.net>

---
+++ lua/conform/formatters/tex-fmt.lua
@@ -0,0 +1,9 @@
+---@type conform.FileFormatterConfig
+return {
+  meta = {
+    url = "https://github.com/WGUNDERWOOD/tex-fmt",
+    description = "An extremely fast LaTeX formatter written in Rust.",
+  },
+  command = "tex-fmt",
+  args = { "-s" },
+}
