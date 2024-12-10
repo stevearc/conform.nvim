4f10486ec108758004f72a07026daa74b8c04efc
Nelson Earle <nelson.earle137@gmail.com>
2024-12-10 13:01:10 -0600

    fix(trim_newlines): meta url

--- lua/conform/formatters/trim_newlines.lua
+++ lua/conform/formatters/trim_newlines.lua
@@ -1,7 +1,7 @@
 ---@type conform.FileLuaFormatterConfig
 return {
   meta = {
-    url = "https://github.com/stevearc/conform.nvim/blob/master/lua/conform/formatters/trim_whitespace.lua",
+    url = "https://github.com/stevearc/conform.nvim/blob/master/lua/conform/formatters/trim_newlines.lua",
     description = "Trim empty lines at the end of the file.",
   },
   format = function(self, ctx, lines, callback)
