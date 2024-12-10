4a4ee72fafc66dcc52eeda9844876a0bc9967cbf
Steven Arcangeli <506791+stevearc@users.noreply.github.com>
2024-12-10 07:21:29 -0800

    doc: unify description format

--- lua/conform/formatters/prettypst.lua
+++ lua/conform/formatters/prettypst.lua
@@ -2,7 +2,7 @@
 return {
   meta = {
     url = "https://github.com/antonWetzel/prettypst",
-    description = "Formatter for Typst",
+    description = "Formatter for Typst.",
   },
   command = "prettypst",
   stdin = true,
