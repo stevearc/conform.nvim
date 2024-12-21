c5cdc475c9100933b9e2f6a4876e8baf5ef2735a
Peter Aronoff <peter@aronoff.org>
2024-12-21 08:09:58 -0500

    feat: add gojq (#598)
    * feat: add gojq
    
    * doc: use common format for description
    
    ---------
    
    Co-authored-by: Steven Arcangeli <506791+stevearc@users.noreply.github.com>

---
+++ lua/conform/formatters/gojq.lua
@@ -0,0 +1,8 @@
+---@type conform.FileFormatterConfig
+return {
+  meta = {
+    url = "https://github.com/itchyny/gojq",
+    description = "Pure Go implementation of jq.",
+  },
+  command = "gojq",
+}
