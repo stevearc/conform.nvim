4c29053b445dcde095999a9defad56fc675a4699
devkvlt <devkvlt@pm.me>
2024-12-21 09:00:38 +0100

    feat: add format-dune-file

---
+++ lua/conform/formatters/format-dune-file.lua
@@ -0,0 +1,9 @@
+---@type conform.FileFormatterConfig
+return {
+  meta = {
+    url = "https://github.com/ocaml/dune",
+    description = "Auto-formatter for Dune files.",
+  },
+  command = "dune",
+  args = { "format-dune-file", "$FILENAME" },
+}
