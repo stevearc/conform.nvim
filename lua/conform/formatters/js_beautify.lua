c591995103682bf2a6aad86ec73a44b9e81a7337
Steven Arcangeli <506791+stevearc@users.noreply.github.com>
2025-01-21 21:02:49 -0800

    doc: shorten description

--- lua/conform/formatters/js_beautify.lua
+++ lua/conform/formatters/js_beautify.lua
@@ -5,7 +5,7 @@ local util = require("conform.util")
 return {
   meta = {
     url = "https://github.com/beautifier/js-beautify",
-    description = [[This little beautifier will reformat and re-indent bookmarklets, ugly JavaScript, unpack scripts packed by Dean Edward’s popular packer, as well as partly deobfuscate scripts processed by the npm package javascript-obfuscator.]],
+    description = "Beautifier for javascript.",
   },
   command = util.from_node_modules(fs.is_windows and "js-beautify.cmd" or "js-beautify"),
   args = { "--file", "-" },
