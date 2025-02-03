ed28bacc6411fe12e281434467b2d298a5146b29
gabriel.winkler <gabriel.winkler@bpm.ch>
2025-02-03 15:19:46 +0100

    Changed path to be without util.from_node_modules

--- lua/conform/formatters/ansible-lint.lua
+++ lua/conform/formatters/ansible-lint.lua
@@ -1,14 +1,14 @@
-local util = require("conform.util")
 ---@type conform.FileFormatterConfig
 return {
   meta = {
     url = "https://github.com/ansible/ansible-lint",
     description = "ansible-lint with --fix",
   },
-  command = util.from_node_modules("ansible-lint"),
+  command = "ansible-lint",
   -- args = { "--fix-to-stdout", "--stdin", "--stdin-filename", "$FILENAME" },
   args = { "-f", "codeclimate", "-q", "--fix=all", "$FILENAME" },
   options = {
     ignore_errors = true,
+    timeout_ms = 10000
   },
 }
