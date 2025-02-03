361b85a39a1b2ea529b4eae68e605cc42e6c9bd8
G4bbix <winkiway@gmail.com>
2025-02-03 09:18:57 +0100

    Added ansible-lint --fix

---
+++ lua/conform/formatters/ansible-lint.lua
@@ -0,0 +1,11 @@
+local util = require("conform.util")
+---@type conform.FileFormatterConfig
+return {
+  meta = {
+    url = "https://github.com/ansible/ansible-lint",
+    description = "ansible-lint with --fix",
+  },
+  command = util.from_node_modules("ansible-lint"),
+  -- args = { "--fix-to-stdout", "--stdin", "--stdin-filename", "$FILENAME" },
+  args = { "-f", "codeclimate", "--fix=all", "$FILENAME" },
+}
