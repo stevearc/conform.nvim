9f669b9e49f34e8eb8c737c21866eed7bdf6d381
Joshua Tye <21010072+catgoose@users.noreply.github.com>
2025-02-03 06:19:41 -0600

    chore: updates meta.url for templ formatters

--- lua/conform/formatters/templ.lua
+++ lua/conform/formatters/templ.lua
@@ -1,7 +1,7 @@
 ---@type conform.FileFormatterConfig
 return {
   meta = {
-    url = "https://templ.guide/commands-and-tools/cli/#formatting-templ-files",
+    url = "https://templ.guide/developer-tools/cli/#formatting-templ-files",
     description = "Formats templ template files.",
   },
   command = "templ",
