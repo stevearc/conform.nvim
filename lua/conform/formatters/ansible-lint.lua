232afc4331cf78dcfafb05185841f3dd8e4f89de
gabriel.winkler <gabriel.winkler@bpm.ch>
2025-02-04 10:34:49 +0100

    Formatting

--- lua/conform/formatters/ansible-lint.lua
+++ lua/conform/formatters/ansible-lint.lua
@@ -5,12 +5,11 @@ return {
     description = "ansible-lint with --fix",
   },
   command = "ansible-lint",
-  -- args = { "--fix-to-stdout", "--stdin", "--stdin-filename", "$FILENAME" },
   args = { "-f", "codeclimate", "-q", "--fix=all", "$FILENAME" },
   options = {
     ignore_errors = true,
-    async = true
+    async = true,
   },
-	stdin = false,
-  exit_codes = { 0, 1, 2, 3, 4 }
+  stdin = false,
+  exit_codes = { 0, 1, 2, 3, 4 },
 }
