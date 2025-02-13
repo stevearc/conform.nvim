8ce4bbd3526e3fd8d190ea3744f8c397044c0dc8
Mads Hougesen <mads@mhouge.dk>
2025-02-13 23:05:58 +0100

    feat: support reformat-gherkin (#652)
    * feat: support reformat-gherkin
    
    * doc: apply unified format to description
    
    ---------
    
    Co-authored-by: Steven Arcangeli <506791+stevearc@users.noreply.github.com>

---
+++ lua/conform/formatters/reformat-gherkin.lua
@@ -0,0 +1,10 @@
+---@type conform.FileFormatterConfig
+return {
+  meta = {
+    url = "https://github.com/ducminh-phan/reformat-gherkin",
+    description = "Formatter for Gherkin language.",
+  },
+  command = "reformat-gherkin",
+  args = { "-" },
+  stdin = true,
+}
