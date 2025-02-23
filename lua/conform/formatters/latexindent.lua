stevearc/conform.nvim

commit f2063898e943893b20aeb30a324d5364ebaddff6

feat(latexindent): support range formatting (#654)

lua/conform/formatters/latexindent.lua
@@ -5,6 +5,9 @@ return {
     description = "A perl script for formatting LaTeX files that is generally included in major TeX distributions.",
   },
   command = "latexindent",
-  args = { "-" },
+  args = {},
+  range_args = function(_, ctx)
+    return { "--lines", ctx.range.start[1] .. "-" .. ctx.range["end"][1] }
+  end,
   stdin = true,
 }

commit 8aa8d19d3a140d727b49f7402192db134f883a51

fix(latexindent): regression in `f2063898e9`
When ran with the `-l` flag and no other filename (as is the case when
passing the file through STDIN), latexindent interprets the argument to
`-l` as the file to be formatted

Eg. `latexindent -l=foo.yaml < bar.tex` overwrites the current buffer
with `foo.yaml` rather than the formatted `bar.tex`

This commit just appends the argument `-` so that STDIN is always used

See: `f2063898e943893b20aeb30a324d5364ebaddff6` (#654)

lua/conform/formatters/latexindent.lua
@@ -5,9 +5,9 @@ return {
     description = "A perl script for formatting LaTeX files that is generally included in major TeX distributions.",
   },
   command = "latexindent",
-  args = {},
+  args = { "-" },
   range_args = function(_, ctx)
-    return { "--lines", ctx.range.start[1] .. "-" .. ctx.range["end"][1] }
+    return { "--lines", ctx.range.start[1] .. "-" .. ctx.range["end"][1], "-" }
   end,
   stdin = true,
 }