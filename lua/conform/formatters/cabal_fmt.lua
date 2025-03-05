4614f14a8cd9458565eb640417f03d6daf11fca7
Steven Arcangeli <506791+stevearc@users.noreply.github.com>
2025-03-04 16:45:39 -0800

    Update cabal_fmt.lua

--- lua/conform/formatters/cabal_fmt.lua
+++ lua/conform/formatters/cabal_fmt.lua
@@ -1,7 +1,7 @@
 return {
   meta = {
     url = "https://hackage.haskell.org/package/cabal-fmt",
-    description = "Format cabal files with cabal-fmt",
+    description = "Format cabal files with cabal-fmt.",
   },
   command = "cabal-fmt",
   args = { "--inplace", "$FILENAME" },
