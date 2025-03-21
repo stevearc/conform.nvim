0823dfb7db7b8bae9f40efa5ae8d962f3f03fb66
Tim Morgan <tim@timmorgan.org>
2025-03-20 21:03:08 -0500

    Fix syntax_tree for ignored files
    Running the command with the actual relative path (not a temp path)
    allows it to honor any --ignore-files configuration in the .streerc file.

--- lua/conform/formatters/syntax_tree.lua
+++ lua/conform/formatters/syntax_tree.lua
@@ -5,7 +5,7 @@ return {
     description = "Syntax Tree is a suite of tools built on top of the internal CRuby parser.",
   },
   command = "stree",
-  stdin = false,
-  args = { "write", "$FILENAME" },
+  stdin = true,
+  args = { "format", "$RELATIVE_FILEPATH" },
   cwd = require("conform.util").root_file({ ".streerc" }),
 }
