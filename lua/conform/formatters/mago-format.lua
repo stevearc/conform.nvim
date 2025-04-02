55712cc08facda54eb950286af9c8b387e0d6371
Mads Hougesen <mads@mhouge.dk>
2025-04-02 20:01:53 +0200

    feat: add support for mago
    Adds support for the  and  commands

---
+++ lua/conform/formatters/mago-format.lua
@@ -0,0 +1,10 @@
+---@type conform.FileFormatterConfig
+return {
+  meta = {
+    url = "https://github.com/carthage-software/mago",
+    description = "Mago is a toolchain for PHP that aims to provide a set of tools to help developers write better code.",
+  },
+  command = "mago",
+  stdin = true,
+  args = { "format", "--stdin-input" },
+}
