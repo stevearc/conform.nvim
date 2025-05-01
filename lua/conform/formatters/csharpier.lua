b93785cfda8faf660ac8ad7a60007fc386a6b308
James Kelly <james.kelly.99@gmail.com>
2025-05-01 16:05:58 +1000

    fix(csharpier): support new cli for csharpier 1.0.x

--- lua/conform/formatters/csharpier.lua
+++ lua/conform/formatters/csharpier.lua
@@ -5,6 +5,6 @@ return {
     description = "The opinionated C# code formatter.",
   },
   command = "dotnet",
-  args = { "csharpier", "--write-stdout" },
+  args = { "csharpier", "format", "$FILENAME", "--write-stdout" },
   stdin = true,
 }
