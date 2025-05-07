7337af71c458c2f68132962f2a04b262951e42ee
Dr.Teagle <65212972+DockterTeagle@users.noreply.github.com>
2025-05-07 06:08:56 -0500

    fix: comma

--- lua/conform/formatters/treefmt.lua
+++ lua/conform/formatters/treefmt.lua
@@ -3,7 +3,7 @@ return {
   meta = {
     url = "https://github.com/numtide/treefmt",
     description = "one CLI to format your repo",
-  }
+  },
   command = "treefmt",
 			args = { "--stdin", "$FILENAME" },
 }
