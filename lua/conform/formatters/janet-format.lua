d8497af9a7967f6f25c19dd7dc218ced15ba13ab
lost22git <65008815+lost22git@users.noreply.github.com>
2025-05-09 23:49:29 +0800

    feat: add janet-format for Janet lang (#706)
    Co-authored-by: lost <lost>

---
+++ lua/conform/formatters/janet-format.lua
@@ -0,0 +1,9 @@
+--@type conform.FileFormatterConfig
+return {
+  meta = {
+    url = "https://github.com/janet-lang/spork",
+    description = "A formatter for Janet code.",
+  },
+  command = "janet-format",
+  stdin = true,
+}
