5bb3cc759bb3d5167051725d01e82b03402849f9
Matt Arau <matt@arau.pro>
2025-06-04 18:06:52 +0200

    feat: add official swift formatter (#718)

--- lua/conform/formatters/swift_format.lua
+++ lua/conform/formatters/swift_format.lua
@@ -1,8 +1,8 @@
 ---@type conform.FileFormatterConfig
 return {
   meta = {
-    url = "https://github.com/apple/swift-format",
-    description = "Swift formatter from apple. Requires building from source with `swift build`.",
+    url = "https://github.com/swiftlang/swift-format",
+    description = "Official Swift formatter. For Swift 6.0 or later prefer setting the `swift` formatter instead.",
   },
   command = "swift-format",
   args = { "$FILENAME", "--in-place" },
