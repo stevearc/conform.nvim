c92e969aa4c6f07b5a03a8632006a52b8179532d
Lukas Nagel <lukasnagel@fastmail.com>
2025-06-22 10:52:27 +0200

    feat: add keep-sorted

---
+++ lua/conform/formatters/keep-sorted.lua
@@ -0,0 +1,10 @@
+---@type conform.FileFormatterConfig
+return {
+  meta = {
+    url = "https://github.com/google/keep-sorted",
+    description = "keep-sorted is a language-agnostic formatter that sorts lines between two markers in a larger file.",
+  },
+
+  command = "keep-sorted",
+  args = { "-" },
+}
