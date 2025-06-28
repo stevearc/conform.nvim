7a9b6c7c9b69d3c89c5594bd9229ae3c4e44cf05
Phùng Thái Hưng <hungpt.a8e@protonmail.com>
2025-06-28 23:57:47 +0700

    feat: add dockerfmt formatter

---
+++ lua/conform/formatters/dockerfmt.lua
@@ -0,0 +1,8 @@
+---@type conform.FileFormatterConfig
+return {
+  meta = {
+    url = "https://github.com/reteps/dockerfmt",
+    description = "Dockerfile formatter. a modern dockfmt.",
+  },
+  command = "dockerfmt",
+}
