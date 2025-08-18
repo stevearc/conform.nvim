0884ca195184f12af6a0377002ea7ac1993b9ecb
Cameron Ring <cameron@cs.stanford.edu>
2025-08-18 15:28:38 -0700

    feat(uncrustify): use project cfg if it exists

--- lua/conform/formatters/uncrustify.lua
+++ lua/conform/formatters/uncrustify.lua
@@ -6,6 +6,15 @@ return {
   },
   command = "uncrustify",
   args = function(self, ctx)
-    return { "-q", "-l", vim.bo[ctx.buf].filetype:upper() }
+    local config_name = "uncrustify.cfg"
+    local args = { "-q", "-l", vim.bo[ctx.buf].filetype:upper() }
+
+    -- Find uncrustify.cfg in the project if it exists
+    local cfg_path = require("conform.util").root_file(config_name)(self, ctx)
+    if cfg_path then
+      table.insert(args, "-c")
+      table.insert(args, cfg_path .. require("conform.fs").sep .. config_name)
+    end
+    return args
   end,
 }
