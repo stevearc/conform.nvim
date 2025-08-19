2e02d0de7c10c7ca464f872b07b38e59a65f07bd
Steven Arcangeli <506791+stevearc@users.noreply.github.com>
2025-08-18 17:25:48 -0700

    doc: update description

--- lua/conform/formatters/qmlformat.lua
+++ lua/conform/formatters/qmlformat.lua
@@ -2,7 +2,7 @@
 return {
   meta = {
     url = "https://doc.qt.io/qt-6//qtqml-tooling-qmlformat.html",
-    description = "A tool that automatically formats QML files",
+    description = "A tool that automatically formats QML files.",
   },
   command = "qmlformat",
   args = { "-i", "$FILENAME" },
