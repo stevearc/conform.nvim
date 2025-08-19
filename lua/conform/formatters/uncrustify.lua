dfab987093f4c9270e7cf2567c49ecbb88da402f
Cameron Ring <cameron@cs.stanford.edu>
2025-08-18 18:09:32 -0700

    refactor(uncrustify): use vim.fs.find to find cfg

--- lua/conform/formatters/uncrustify.lua
+++ lua/conform/formatters/uncrustify.lua
@@ -5,15 +5,14 @@ return {
     description = "A source code beautifier for C, C++, C#, ObjectiveC, D, Java, Pawn and Vala.",
   },
   command = "uncrustify",
-  args = function(self, ctx)
-    local config_name = "uncrustify.cfg"
+  args = function(_, ctx)
     local args = { "-q", "-l", vim.bo[ctx.buf].filetype:upper() }
 
     -- Find uncrustify.cfg in the project if it exists
-    local cfg_path = require("conform.util").root_file(config_name)(self, ctx)
+    local cfg_path = vim.fs.find("uncrustify.cfg", { upward = true, path = ctx.dirname })[1]
     if cfg_path then
       table.insert(args, "-c")
-      table.insert(args, cfg_path .. require("conform.fs").sep .. config_name)
+      table.insert(args, cfg_path)
     end
     return args
   end,
