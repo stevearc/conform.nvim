8176edbb1e84d94f693491fd92ad16320eb068d8
Kevin Nitro <trannguyenthaibinh46@gmail.com>
2025-09-24 08:11:58 +0700

    fix(d2): format with temp file instead of stdin

--- lua/conform/formatters/d2.lua
+++ lua/conform/formatters/d2.lua
@@ -5,5 +5,9 @@ return {
     description = "D2 is a modern diagram scripting language that turns text to diagrams.",
   },
   command = "d2",
-  args = { "fmt", "-" },
+  stdin = false,
+  args = {
+    "fmt",
+    "$FILENAME",
+  },
 }
