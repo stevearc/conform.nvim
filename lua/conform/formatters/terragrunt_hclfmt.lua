c3fe5a073b21731317ebd7f3ffe8a632bafab163
Thomas Sjögren <thomas@sjogrens.dk>
2025-09-27 18:03:45 +0200

    fix(terragrunt): use new format subcommand (#766)

--- lua/conform/formatters/terragrunt_hclfmt.lua
+++ lua/conform/formatters/terragrunt_hclfmt.lua
@@ -5,7 +5,7 @@ return {
     description = "Format hcl files into a canonical format.",
   },
   command = "terragrunt",
-  args = { "hclfmt", "--terragrunt-hclfmt-file", "$FILENAME" },
+  args = { "hcl", "fmt", "--file", "$FILENAME" },
   stdin = false,
   condition = function(self, ctx)
     return vim.fs.basename(ctx.filename) ~= "terragrunt.hcl"
