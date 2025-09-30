8a2ad2a1ed5b9b43f29378f342472badaa90f511
Steven Arcangeli <506791+stevearc@users.noreply.github.com>
2025-09-29 21:24:00 -0700

    doc: normalize description

--- lua/conform/formatters/meson.lua
+++ lua/conform/formatters/meson.lua
@@ -2,7 +2,7 @@
 return {
   meta = {
     url = "https://github.com/mesonbuild/meson",
-    description = "Format meson source file",
+    description = "Format meson source files.",
   },
   command = "meson",
   args = { "format", "--source-file-path", "$FILENAME", "-" },
