f1d3a01809d71fc6a31a9107a039aca80e784329
TomIO <tomjo00@web.de>
2025-10-04 05:04:57 +0200

    feat(taplo): respect lsp options See: https://taplo.tamasfe.dev/cli/usage/formatting.html

--- lua/conform/formatters/taplo.lua
+++ lua/conform/formatters/taplo.lua
@@ -5,5 +5,18 @@ return {
     description = "A TOML toolkit written in Rust.",
   },
   command = "taplo",
-  args = { "format", "--stdin-filepath", "$FILENAME", "-" },
+  args = function()
+    ---@type lsp.LSPObject?
+    local taplo_config = vim.tbl_get(vim.lsp.config, "taplo", "settings", "formatter")
+
+    local ret = { "format" }
+    if taplo_config ~= nil then
+      for k, v in pairs(taplo_config) do
+        vim.list_extend(ret, { "-o", ("%s=%s"):format(k, v) })
+      end
+    end
+    vim.list_extend(ret, { "--stdin-filepath", "$FILENAME", "-" })
+
+    return ret
+  end,
 }
