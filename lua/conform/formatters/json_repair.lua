be50cf36acea84c17fe269eb228d1b2f53bca7f8
Yuma <yuma.fuu05@gmail.com>
2025-10-08 12:47:22 +0900

    feat: add json_repair formatter (#787)
    * add:json_repair
    
    * update:args

---
+++ lua/conform/formatters/json_repair.lua
@@ -0,0 +1,9 @@
+---@type conform.FileFormatterConfig
+return {
+  meta = {
+    url = "https://github.com/mangiucugna/json_repair",
+    description = "A python module to repair invalid JSON from LLMs.",
+  },
+  command = "json_repair",
+  args = {},
+}
