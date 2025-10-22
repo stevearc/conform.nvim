97c27bdabd76309959f97fbab6fff4903fc4e7dd
Acid Bong <acidbong@tilde.club>
2025-10-22 22:19:25 +0300

    doc(nixfmt): update description (#792)
    Nixfmt has reached v1 and is since considered stable

--- lua/conform/formatters/nixfmt.lua
+++ lua/conform/formatters/nixfmt.lua
@@ -2,7 +2,7 @@
 return {
   meta = {
     url = "https://github.com/NixOS/nixfmt",
-    description = "The official (but not yet stable) formatter for Nix code.",
+    description = "The official formatter for Nix code.",
   },
   command = "nixfmt",
 }
