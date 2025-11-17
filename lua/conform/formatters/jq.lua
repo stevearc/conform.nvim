1bf8b5b9caee51507aa51eaed3da5b0f2595c6b9
Jochen Sprickerhof <github@jochen.sprickerhof.de>
2025-11-17 19:34:29 +0100

    fix(jq): limit indetion to [-1,7] (#807)
    According to the documentation jq only accepts indention between -1 and
    7.
    
    Regression of 235dd79.

--- lua/conform/formatters/jq.lua
+++ lua/conform/formatters/jq.lua
@@ -6,6 +6,6 @@ return {
   },
   command = "jq",
   args = function(_, ctx)
-    return { "--indent", ctx.shiftwidth }
+    return { "--indent", math.max(-1, math.min(7, ctx.shiftwidth)) }
   end,
 }
