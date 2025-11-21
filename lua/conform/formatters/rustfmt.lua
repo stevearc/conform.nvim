b5a938a64ecbb7e1efc879f1195055d6adffa828
Chen Mulong <chenmulong@gmail.com>
2025-11-21 14:07:42 +0800

    feat: Add "nightly" option to rustfmt

--- lua/conform/formatters/rustfmt.lua
+++ lua/conform/formatters/rustfmt.lua
@@ -10,12 +10,18 @@ return {
   options = {
     -- The default edition of Rust to use when no Cargo.toml file is found
     default_edition = "2021",
+    -- Set to true to use the nightly version of rustfmt
+    nightly = false,
   },
   args = function(self, ctx)
     local args = { "--emit=stdout" }
     local edition = util.parse_rust_edition(ctx.dirname) or self.options.default_edition
     table.insert(args, "--edition=" .. edition)
 
+    if self.options.nightly then
+      -- "+nightly" must be the first argument
+      table.insert(args, 1, "+nightly")
+    end
     return args
   end,
   cwd = util.root_file({
