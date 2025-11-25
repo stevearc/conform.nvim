917f36ad51ca0c8b0ec97bdf9641dafb79e04b4c
Steven Arcangeli <506791+stevearc@users.noreply.github.com>
2025-11-24 22:27:13 -0800

    Fix description formatting in mh_style.lua

--- lua/conform/formatters/mh_style.lua
+++ lua/conform/formatters/mh_style.lua
@@ -2,7 +2,7 @@
 return {
   meta = {
     url = "https://github.com/florianschanda/miss_hit",
-    description = "A simple coding style checker and code formatter for MATLAB or Octave code",
+    description = "A simple coding style checker and code formatter for MATLAB or Octave code.",
   },
   command = "mh_style",
   args = { "--fix", "$FILENAME" },
