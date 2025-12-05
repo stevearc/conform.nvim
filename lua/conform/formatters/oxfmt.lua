e2e0fcc48ba6a3baf26a211ccfd90a66d7813bda
François Lachèse <francois.lachese@gmail.com>
2025-12-05 15:27:19 +0100

    fix: cwd detection
    without cwd detection configuration files would be ignored in subdirectories

--- lua/conform/formatters/oxfmt.lua
+++ lua/conform/formatters/oxfmt.lua
@@ -1,5 +1,11 @@
 local util = require("conform.util")
 
+local config_file_names = {
+  -- https://oxc.rs/docs/guide/usage/formatter.html#configuration-file
+  ".oxfmtrc.json",
+  ".oxfmtrc.jsonc",
+}
+
 ---@type conform.FileFormatterConfig
 return {
   meta = {
@@ -9,4 +15,5 @@ return {
   command = util.from_node_modules("oxfmt"),
   args = { "$FILENAME" },
   stdin = false,
+  cwd = require("conform.util").root_file(config_file_names),
 }
