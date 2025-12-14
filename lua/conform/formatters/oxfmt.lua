4bb461babc231e6c9d5cae418e2ab22ed772dd70
François Lachèse <33827360+FLchs@users.noreply.github.com>
2025-12-14 08:44:31 +0100

    feat: add Oxfmt (#818)
    * feat: add Oxfmt
    
    * docs: Add reference to Oxfmt in readme
    
    * fix: cwd detection
    
    without cwd detection configuration files would be ignored in subdirectories
    
    * doc: simplify description of Oxfmt
    
    ---------
    
    Co-authored-by: Steven Arcangeli <506791+stevearc@users.noreply.github.com>

---
+++ lua/conform/formatters/oxfmt.lua
@@ -0,0 +1,19 @@
+local util = require("conform.util")
+
+local config_file_names = {
+  -- https://oxc.rs/docs/guide/usage/formatter.html#configuration-file
+  ".oxfmtrc.json",
+  ".oxfmtrc.jsonc",
+}
+
+---@type conform.FileFormatterConfig
+return {
+  meta = {
+    url = "https://github.com/oxc-project/oxc",
+    description = "A Prettier-compatible code formatter.",
+  },
+  command = util.from_node_modules("oxfmt"),
+  args = { "$FILENAME" },
+  stdin = false,
+  cwd = require("conform.util").root_file(config_file_names),
+}
