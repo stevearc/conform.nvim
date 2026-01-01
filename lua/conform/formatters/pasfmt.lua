8314f4c9e205e7f30b62147069729f9a1227d8bf
Mads Hougesen <mads@mhouge.dk>
2026-01-01 14:18:34 +0100

    feat: add support for pasfmt (#841)
    * feat(tools): add support for pasfmt
    
    * refactor: use stdin

---
+++ lua/conform/formatters/pasfmt.lua
@@ -0,0 +1,10 @@
+---@type conform.FileFormatterConfig
+return {
+  meta = {
+    url = "https://github.com/integrated-application-development/pasfmt",
+    description = "Delphi code formatter.",
+  },
+  command = "pasfmt",
+  args = {},
+  stdin = true,
+}
