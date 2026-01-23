5f53fd73a5787bdb2b9bc60b91be7eb0e9d9ba14
George Harker <george@george-graphics.co.uk>
2026-01-23 14:45:28 -0800

    add support for topiary

---
+++ lua/conform/formatters/topiary.lua
@@ -0,0 +1,50 @@
+local util = require("conform.util")
+
+-- Map Neovim filetypes to Topiary language names
+local ft_to_language = {
+    bash = "bash",
+    sh = "bash",
+    css = "css",
+    json = "json",
+    nickel = "nickel",
+    ocaml = "ocaml",
+    rust = "rust",
+    toml = "toml",
+    query = "tree_sitter_query",
+    wit = "wit",
+    zsh = "zsh",
+}
+
+---@type conform.FileFormatterConfig
+return {
+    meta = {
+        url = "https://topiary.tweag.io",
+        description = "A universal code formatter based on tree-sitter, supporting multiple languages with a consistent formatting style.",
+    },
+    command = "topiary",
+    condition = function(self, ctx)
+        local ft = vim.bo[ctx.buf].filetype
+        local language = self.options.language or ft_to_language[ft]
+        return language ~= nil
+    end,
+    args = function(self, ctx)
+        local ft = vim.bo[ctx.buf].filetype
+        local language = self.options.language or ft_to_language[ft]
+
+        return {
+            "format",
+            "--language",
+            language,
+        }
+    end,
+    options = {
+        -- Override the detected language for a specific filetype
+        -- Example: { language = "bash" }
+        language = nil,
+    },
+    stdin = true,
+    cwd = util.root_file({
+        ".topiary.toml",
+        "topiary.toml",
+    }),
+}
